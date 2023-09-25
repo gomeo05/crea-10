415ad75f14148a396714f0ed4adeb798bda7ff15
Dwayne Dantes <139754226+gomeo05@users.noreply.github.com>
2023-09-25 08:54:18 +0700

    Create beeef.py

---
+++ beeef.py
@@ -0,0 +1,12 @@
+import json
+import time
+from typing import Any, Dict, List, Union
+import torch
+import bmtrain as bmt
+import os
+from cpm_live.arguments import get_args
+
+from cpm_live.models import CPMBee, CPMBeeConfig
+from cpm_live.tokenizers import CPMBeeTokenizer
+from cpm_live.utils import allgather_objects, LogManager
+from cpm_live.training_tasks.bee import MixedDataset
